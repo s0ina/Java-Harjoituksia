7e46138121a94a7c36840a5c8d2f2ce9fd04223f
Sonja <sonja.sini.maria@gmail.com>
2026-08-25 18:40:11 +0300

    Variables exercise

---
+++ src/Harjoitukset/Variables.java
@@ -0,0 +1,17 @@
+package harjoitukset;
+
+public class Variables {
+	public static void main(String[]args) {
+		String firstName = "Jane";
+		String lastName = "Doe";
+		String fullName = (firstName +" "+ lastName);
+		int x = 6;
+		int y = 3;
+		int z = 9;
+		System.out.println(fullName);
+		System.out.println("The sum of x and y is " + (x + y));
+		System.out.println("The sum is not " + x + y);
+		System.out.println(x +" "+ y +" "+ z);
+
+	}
+}
