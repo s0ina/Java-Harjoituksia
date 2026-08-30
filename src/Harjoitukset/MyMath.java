f8e04a98bc15534e9fae1716d4c34701c8fbe1b9
Sonja <sonja.sini.maria@gmail.com>
2026-08-30 12:33:50 +0300

    Java Math exercise

---
+++ src/Harjoitukset/MyMath.java
@@ -0,0 +1,24 @@
+package harjoitukset;
+
+public class MyMath {
+	public static void main(String[]args) {
+		//Java Math
+		System.out.println(Math.max(3, 6));
+		System.out.println(Math.min(3, 6));
+		System.out.println(Math.sqrt(64));
+		System.out.println(Math.abs(-3.6));
+		System.out.println(Math.pow(6, 2));
+		System.out.println(Math.random());
+		
+		//Rounding methods
+		//Nearest integer
+		System.out.println(Math.round(3.5));
+		
+		//rounds up (returns the smallest integer greater than or equal to x)
+		System.out.println(Math.ceil(6.2));
+		
+		//rounds down (returns the largest integer less than or equal to x)
+		System.out.println(Math.floor(6.2));
+
+	}
+}
---
+++ src/Harjoitukset/Strings.java
@@ -0,0 +1,8 @@
+package harjoitukset;
+
+public class Strings {
+	public static void main(String[]args) {
+		String txt = "\"Learning Java is cool\"";
+		System.out.println("The length of " +txt + " is " + txt.length());
+	}
+}
