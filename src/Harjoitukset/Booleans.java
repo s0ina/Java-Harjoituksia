6cb6b7dd845325bac920d860514aaab5265bbef7
Sonja <sonja.sini.maria@gmail.com>
2026-09-02 18:40:12 +0300

    Booleans exercise

---
+++ src/Harjoitukset/Booleans.java
@@ -0,0 +1,22 @@
+package harjoitukset;
+import java.util.Scanner;
+
+public class Booleans {
+	public static void main(String[] args) {
+	//Combining some scanner + if/else practice here as well
+	Scanner userNum = new Scanner(System.in);
+	System.out.println("Enter age:");
+	int age = userNum.nextInt();
+	int votingAge = 18;
+	
+	if (age >= votingAge) {
+		System.out.println(age >= votingAge);
+		System.out.println("Old enough to vote!");
+	}
+	else {
+		System.out.println(age >= votingAge);
+		System.out.println("Not old enough to vote.");
+	}
+
+	}
+}
