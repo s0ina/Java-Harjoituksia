4d5c4bb427cb3f897f5b10fd829b5966f0fa2ab2
Sonja <sonja.sini.maria@gmail.com>
2026-09-03 16:05:07 +0300

    Conditions exercise

---
+++ src/Harjoitukset/Conditions.java
@@ -0,0 +1,19 @@
+package harjoitukset;
+import java.util.Scanner;
+
+public class Conditions {
+	public static void main(String[] args ) {
+		//Short Hand If...Else
+		Scanner age = new Scanner(System.in);
+		System.out.println("Enter age:");
+		int voterAge = age.nextInt();
+							//if
+		String message = (voterAge < 18) ? "Not old enough to vote"
+							//else if
+						:(voterAge == 18) ? "You're 18! Old enough to vote"
+							//else
+						: "Old enough to vote";
+		System.out.println(message);
+	}
+
+}
